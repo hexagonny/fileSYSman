1f0ae4e38201685f259ecafc8e18b40778a963ff
hexagonny <168445378+hexagonny@users.noreply.github.com>
2025-02-11 00:54:26 +0800

    Add files via upload
    Organized .cpp and .h files.

---
+++ FULL/src/logMessages.cpp
@@ -0,0 +1,75 @@
+#include "logMessages.h"
+
+using namespace std;
+
+//  Function to set the console text color using ANSI escape codes.
+void setColor(int textColor)
+{
+    cout<<"\033["<<textColor<<"m";
+}
+//  Function to reset the console color
+void resetColor() { cout<<"\033[0m"; }
+
+//  This function exists for better output handling.
+void sleep(int miliseconds)
+{
+    this_thread::sleep_for(chrono::milliseconds(miliseconds));
+}
+
+Logger logger;
+
+void Logger::logAction(const string& message)
+{
+    cout<<message<<'\n';
+}
+
+void Logger::logSuccess(const string& message)
+{
+    setColor(92); cout.flush();
+    cout<<message<<'\n'; resetColor();
+}
+
+void Logger::logMoved(const string& message)
+{
+    setColor(93); cout.flush();
+    cout<<message<<'\n'; resetColor();
+    filesMoved++;
+}
+
+void Logger::logRemoved(const string& message)
+{
+    setColor(93); cout.flush();
+    cout<<message<<'\n'; resetColor();
+    foldersRemoved++;
+}
+
+void Logger::logCreated(const string& message)
+{
+    setColor(92); cout.flush();
+    cout<<message<<'\n'; resetColor();
+    foldersCreated++;
+}
+
+void Logger::logError(const string& message)
+{
+    setColor(91); cerr.flush();
+    cerr<<message<<'\n'; resetColor();
+    errors++;
+}
+
+void Logger::logWarning(const string& message)
+{
+    setColor(95); cerr.flush();
+    cerr<<message<<'\n'; resetColor();
+    warnings++;
+}
+
+void Logger::displaySummary()
+{
+    cout<<"\nSummary:\n\n"
+        <<setw(17)<<left<<"Files Moved: "    <<filesMoved<<'\n'
+        <<setw(17)<<left<<"Folders Removed: "<<foldersRemoved<<'\n'
+        <<setw(17)<<left<<"Folders Created: "<<foldersCreated<<'\n'
+        <<setw(17)<<left<<"Warnings: "       <<warnings<<'\n'
+        <<setw(17)<<left<<"Errors: "         <<errors<<"\n\n";
+}
